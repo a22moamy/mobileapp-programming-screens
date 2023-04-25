d9cfe350b6f85b69316cd051c077a9e1fb71917f
a22moamy <a22moamy@student.his.se>
2023-04-25 16:34:20 +0200

    Implementerade tre textviews i secondActivity, varav två av dem får sitt värde genom setText()

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -2,5 +2,25 @@ package com.example.screens;
 
 import android.app.Activity;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
+
 public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        TextView namn = findViewById(R.id.namnRespons);
+
+        TextView email = findViewById(R.id.emailRespons);
+
+        namn.setText("Heyyy");
+
+        email.setText("Hello everyone");
+
+    }
 }
+
